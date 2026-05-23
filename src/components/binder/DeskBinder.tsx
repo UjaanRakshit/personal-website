b6b5ef3848658f94291fe0e3d960c60fa4ef39e4
Ujaan Rakshit <ujaanrakshit@gmail.com>
2026-05-22 23:40:00 -0400

    Reframe desktop binder as an open book with leather cover on the left

--- src/components/binder/DeskBinder.tsx
+++ src/components/binder/DeskBinder.tsx
@@ -3,22 +3,22 @@
 import type { BinderProps } from './types';
 import PageArea from './PageArea';
 import SideTabs from './SideTabs';
-import Cover from './Cover';
 import Rings from './Rings';
 
 export default function DeskBinder(props: BinderProps) {
   return (
-    <div className="desk-stage h-screen overflow-hidden flex justify-center">
+    <div className="desk-stage h-screen overflow-hidden flex justify-center pt-6">
       <div
-        className="binder relative"
-        style={{ width: 'min(880px, 95vw)', height: 'calc(100vh + 220px)', marginTop: 36 }}
+        className="open-book"
+        style={{
+          width: 'min(1120px, 96vw)',
+          height: 'calc(100vh + 220px)',
+        }}
       >
-        <Cover />
+        <div aria-hidden className="leather-cover" />
         <Rings />
-        <div className="relative h-full">
-          <PageArea {...props} variant="desktop" />
-          <SideTabs activeSection={props.activeSection} />
-        </div>
+        <PageArea {...props} variant="desktop" />
+        <SideTabs activeSection={props.activeSection} />
       </div>
     </div>
   );
